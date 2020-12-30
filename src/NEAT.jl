5634eade793569bbe1ce2688a8d1b9a677b80f10
Totti95U <u.mitomayu984@gmail.com>
2020-12-31 01:04:00 +0900

    Populationを生成する関数の追加、細かな修正

--- src/NEAT.jl
+++ src/NEAT.jl
@@ -31,25 +31,25 @@ mutable struct Model{T<:Number}
     output_size::Int
     nodes_num::Int
     weights_num::Int
-    generation::Int
 end
 
 # モデルを作る
-function Model(input_size::Int, output_size::Int; T=Float32, rate=0.5, rng=MersenneTwister(0))
-    nodes = []
+function Model(input_size::Int, output_size::Int; T=Float32, rate=0.5, rng=MersenneTwister())
+    nodes = Array{Node, 1}()
     nodes_num = input_size + output_size
     weights_num = 0
-
+    
     # InputNodeの設定
     for i in 1:input_size
         # 出力へつなげたり繋げなかったりする
-        for i in (input_size+1):nodes_num
-            if rand(rng) < rate 
-                push!(targets, i)
+        targets = Array{Int, 1}()
+        for j in (input_size+1):nodes_num
+            if rand(rng) < rate
+                push!(targets, j)
                 weights_num += 1
             end
         end
-        node = InputNode(i, T(0), targets, 2rand(T, length(targets)).-1)
+        node = InputNode(i, T(0), targets, 2rand(rng, T, length(targets)).-1)
         push!(nodes, node)
     end
 
@@ -59,7 +59,7 @@ function Model(input_size::Int, output_size::Int; T=Float32, rate=0.5, rng=Merse
         push!(nodes, node)
     end
 
-    return Model{T}(nodes, input_size, output_size, nodes_num, weights_num, 1)
+    return Model{T}(nodes, input_size, output_size, nodes_num, weights_num)
 end
 
 # 全ノードのstateをbiasにする
@@ -73,7 +73,7 @@ end
 # モデルに入力を入れて計算する
 function (model::Model{T})(input::Array{T, 1}) where T <: Number
     reset!(model)
-    middlenode_ids = []
+    middlenode_ids = Array{Int, 1}()
     
     # InputNodeからの出力を計算
     for i in 1:model.input_size
@@ -117,42 +117,151 @@ mutable struct Population{T<:Number}
 end
 
 
-function node_distane(model1::Model, model2::Model)
+function node_distance(model1::Model, model2::Model; compatibility_weight_coefficient=0.5, compatibility_disjoint_coefficient=0.5)
+    disjoint_nodes = abs(model1.nodes_num - model2.nodes_num) * compatibility_disjoint_coefficient
 
+    bias_distance = 0.0
+    for node1 in model1.nodes
+        if typeof(node1) <: InputNode
+            continue
+        end
+        for node2 in model2.nodes
+            if typeof(node2) <: InputNode
+                continue
+            end
+            if node1.id == node2.id
+                bias_distance += abs(node1.bias - node2.bias)
+                break
+            end
+        end
+    end
+    bias_distance *= compatibility_weight_coefficient
+
+    return (disjoint_nodes+bias_distance)/max(model1.nodes_num, model2.nodes_num)
 end
 
-function connection_distance(model1::Model, model2::Model)
+function connection_distance(model1::Model, model2::Model; compatibility_weight_coefficient=0.5, compatibility_disjoint_coefficient=0.5)
+    disjoint_connections = abs(model1.weights_num - model2.weights_num) * compatibility_disjoint_coefficient
 
+    # nodeの数の6乗ぐらいの計算量のクソアルゴリズムなので改善したい
+    weight_distance = 0.0
+    for node1 in model1.nodes
+        if typeof(node1) <: OutputNode
+            continue
+        end
+        for node2 in model2.nodes
+            if typeof(node2) <: OutputNode
+                continue
+            end
+            if node1.id == node2.id
+
+                for i in 1:length(node1.targets)
+                    target1 = node1.targets[i]
+                    for j in 1:length(node2.targets)
+                        target2 = node2.targets[j]
+                        if target1 == target2
+                            weight_distance += abs(node1.weights[i] - node2.weights[j])
+                            break
+                        end
+                    end
+                end
+                break
+
+            end
+        end
+    end
+    weight_distance *= compatibility_weight_coefficient
+
+    return (disjoint_connections + weight_distance) / max(model1.weights_num, model2.weights_num)
 end
 
 # 種を分ける (最初の世代の場合)
-function Population(models::Array{Model, 1}; compatibility_threshold=3.0<:AbstractFloat, compatibility_disjoint_coefficient=1.0<:AbstractFloat)
+function Population(models::Array{Model{T}, 1}; compatibility_threshold=3.0, compatibility_disjoint_coefficient=0.5, compatibility_weight_coefficient=0.5) where T
     species_set_toReturn = [Species(1, models[1].nodes_num, models[1].weights_num, [1])]
-    species_set_toExamine = copy(species_set_toExamine)
-    species_count = 0
-    unexamined = [i for i in 1:length(models)]
+    species_set_toExamine = copy(species_set_toReturn)
+    unexamined = [i for i in 2:length(models)]
 
-    while length(species_set_toExamine) > 0
+    while length(unexamined) > 0
         current_species = pop!(species_set_toExamine)
-        species_count += 1
+        for i in copy(unexamined)
+            distance = node_distance(models[current_species.representative_id], models[i], 
+                            compatibility_disjoint_coefficient=compatibility_disjoint_coefficient,
+                            compatibility_weight_coefficient=compatibility_weight_coefficient) 
+                        + connection_distance(models[current_species.representative_id], models[i],
+                            compatibility_disjoint_coefficient=compatibility_disjoint_coefficient,
+                            compatibility_weight_coefficient=compatibility_weight_coefficient)
 
-        for i in unexamined
-            distance = node_distance(models[current_species.representative_id], models[i]) + connection_distance(models[current_species.representative_id], models[i])
             if (distance > compatibility_threshold && length(species_set_toExamine) == 0)
                 new_species = Species(i, models[i].nodes_num, models[i].weights_num, [i])
                 push!(species_set_toReturn, new_species)
                 push!(species_set_toExamine, new_species)
+                deleteat!(unexamined, findfirst(x->x==i, unexamined))
             elseif (distance <= compatibility_threshold)
-                push!(species_set_toReturn.model_ids, i)
-                delete!(unexamined, i)
+                push!(species_set_toReturn[end].model_ids, i)
+                deleteat!(unexamined, findfirst(x->x==i, unexamined))
             end
         end
     end
 
-    return Population(models, species_set_toReturn, length(species_set_toReturn), models[1].generation)
+    return Population(models, species_set_toReturn, length(species_set_toReturn), 1)
 end
 
 # 種を分ける（最初の世代じゃない場合）
-function Population(models::Array{Model, 1}, previous_population::Population; compatibility_threshold=3.0<:AbstractFloat, compatibility_disjoint_coefficient=1.0<:AbstractFloat)
+function Population(models::Array{Model{T}, 1}, previous_population::Population; 
+    compatibility_threshold=3.0, compatibility_disjoint_coefficient=1.0, compatibility_weight_coefficient=0.5) where T
 
+    species_set_toReturn = Array{Species, 1}()
+    previous_species_set_toExamine = copy(previous_population.species_set)
+    unexamined = [i for i in 1:length(models)]
+
+    # 前の世代の種に含まれるかどうか調査する
+    for current_species in previous_species_set_toExamine
+        isnew = true
+
+        for i in unexamined
+            distance = node_distance(previous_population.models[current_species.representative_id], models[i], 
+                            compatibility_disjoint_coefficient=compatibility_disjoint_coefficient,
+                            compatibility_weight_coefficient=compatibility_weight_coefficient)
+                        + connection_distance(previous_population.models[current_species.representative_id], models[i],
+                            compatibility_disjoint_coefficient=compatibility_disjoint_coefficient,
+                            compatibility_weight_coefficient=compatibility_weight_coefficient)
+
+            if (distance <= compatibility_threshold && isnew)
+                new_species = Species(i, models[i].nodes_num, models[i].weights_num, [i])
+                push!(species_set_toReturn, new_species)
+                deleteat!(unexamined, findfirst(x->x==i, unexamined))
+                isnew = false
+            elseif (distance <= compatibility_threshold)
+                push!(species_set_toReturn[end].model_ids, i)
+                deleteat!(unexamined, findfirst(x->x==i, unexamined))
+            end
+        end
+    end
+
+    # 新種を分ける
+    species_set_toExamine = Array{Species, 1}()
+    while length(unexamined) > 0
+        new_species = Species(unexamined[1], models[unexamined[1]].nodes_num, models[unexamined[1]].weights_num, [unexamined[1]])
+        popfirst!(unexamined)
+
+        for i in unexamined
+            distance = node_distance(models[new_species.representative_id], models[i], 
+                            compatibility_disjoint_coefficient=compatibility_disjoint_coefficient,
+                            compatibility_weight_coefficient=compatibility_weight_coefficient)
+                        + connection_distance(models[new_species.representative_id], models[i],
+                            compatibility_disjoint_coefficient=compatibility_disjoint_coefficient,
+                            compatibility_weight_coefficient=compatibility_weight_coefficient)
+
+            if (distance <= compatibility_threshold)
+                push!(new_species.model_ids, i)
+                deleteat!(unexamined, findfirst(x->x==i, unexamined))
+            end
+        end
+        push!(species_set_toReturn, new_species)
+    end
+
+    return Population(models, species_set_toReturn, length(species_set_toReturn), previous_population.generation+1)
 end
+
+models = [Model(4, 2) for _ in 1:20]
+Population(models);
